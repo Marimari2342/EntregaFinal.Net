1302c309b0854525e2454b6f14a74e37121413a2
Consuelo Vereau <168292313+ConsueloVT@users.noreply.github.com>
2024-06-17 22:24:54 -0300

    commit

--- SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03204c54a4a1c86299b54fde5496598e22ff5b73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cf7f720ce87c01fd7fc93b07bc6f6382841f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

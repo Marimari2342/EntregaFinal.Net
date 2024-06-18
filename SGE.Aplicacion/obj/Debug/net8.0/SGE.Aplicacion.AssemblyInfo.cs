5b71557318244b794e5da1180aa3b08f443b4cd4
Laura <leguizamonlauram@gmail.com>
2024-06-17 23:01:24 -0300

    cmmit

--- SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
+++ SGE.Aplicacion/obj/Debug/net8.0/SGE.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cf7f720ce87c01fd7fc93b07bc6f6382841f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46eb348fa9c351974b54d5d4f02302a5845b6ac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

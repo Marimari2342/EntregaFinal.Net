96d7ebf2e1311091b279e49889c1d47b002098fe
Laura <leguizamonlauram@gmail.com>
2024-06-19 15:12:11 -0300

    pull

--- SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1500f149a52197d0622d93d5e8bf0546d83377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ca7957bc6abc886b39ec013d1b775b83c39992")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

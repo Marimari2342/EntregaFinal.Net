5ccb86323589f840086474ea3d923e8373e96dde
Laura <leguizamonlauram@gmail.com>
2024-06-19 18:30:06 -0300

    algo

--- SGE.Aplicacion/CasosDeUso/CasoDeUsoAgregarUsuario.cs
+++ SGE.Aplicacion/CasosDeUso/CasoDeUsoAgregarUsuario.cs
@@ -1,27 +1,17 @@
 ﻿using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Interfaces;
 namespace SGE.Aplicacion.CasosDeUso;
-
-public class CasoDeUsoAgregarUsuario(IUsuarioRepositorio repositorio): CasoDeUsoUsuario(repositorio)
+public class CasoDeUsoAgregarUsuario(IUsuarioRepositorio repositorio, IHashService servicio ):CasoDeusoUsuario(repositorio)
 {
-    public void Ejecutar (Usuario usuario){
-        //Aquí se puede insertar codigo de vvalidacion  de usuario
-        Repositorio.AgregarUsuario(usuario); 
-    }
-}
-/*public class CasoDeUsoAgregarUsuario(IUsuarioRepositorio repositorio, IHashService hashService): CasoDeusoUsuario(repositorio)
-{
-    private readonly IUsuarioRepositorio _repositorio;
-    private readonly IHashService _hashService;
 
     public void Ejecutar(Usuario usuario)
     {
         // Crear el hash y la sal para la contraseña
-        var (hash, salt) = _hashService.CreateHash(usuario.Contraseña);
+        var (hash, salt) = servicio.CreateHash(usuario.Contraseña);
         usuario.HashContraseña = hash;
         usuario.SalContraseña = salt;
         usuario.Contraseña = ""; // Limpiar la contraseña original
 
-        _repositorio.AgregarUsuario(usuario);
+        repositorio.AgregarUsuario(usuario);
     }
-}*/
+}
--- SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
+++ SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a1aaf9eec887e7297e094586ddc572f9f55043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cd53aebfdd485f1bb16d8ee45843d25f275870")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f20f8d1b2f88da1954189deabe66f65c5cc66fc2
Laura <leguizamonlauram@gmail.com>
2024-06-20 15:20:07 -0300

    commit para pushear problemas con el path de los archivos.

--- SGE.Aplicacion/Entidades/Usuario.cs
+++ SGE.Aplicacion/Entidades/Usuario.cs
@@ -9,13 +9,13 @@ public class Usuario
 
     public string Apellido{ get; set;}="";
     public string CorreoElectronico { get; set;}="";
-    public string Contraseña { get; set;}="";
+    public string Contraseña { get; set;}=""; //Me parece que no deberíamos guardar la contraseña sino el Hash y sal nomás.
 
-   // Nuevos campos para almacenar el hash y la sal de la contraseña
-        public string HashContraseña { get; set; } = "";
-        /*Sal se refiere a una cadena de datos aleatoria que se añade a la contraseña antes de calcular su hash
-        La sal se genera de manera aleatoria y se combina con la contraseña antes de calcular su hash*/
-        public string SalContraseña { get; set; } = "";
+    // Nuevos campos para almacenar el hash y la sal de la contraseña
+    public string HashContraseña { get; set; } = "";
+    /*Sal se refiere a una cadena de datos aleatoria que se añade a la contraseña antes de calcular su hash
+    La sal se genera de manera aleatoria y se combina con la contraseña antes de calcular su hash*/
+    public string SalContraseña { get; set; } = "";
 
     public List<Permiso> ListaPermisos{ get; set;} = new List<Permiso>();
 }

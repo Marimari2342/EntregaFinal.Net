688149d0bb84db000c3c15b8c7e45f89660e9785
Laura <leguizamonlauram@gmail.com>
2024-06-20 20:42:00 -0300

    commit

--- SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20f8d1b2f88da1954189deabe66f65c5cc66fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcb60a4f04394255370de4957f1a1d5d697db72")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
+++ SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20f8d1b2f88da1954189deabe66f65c5cc66fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcb60a4f04394255370de4957f1a1d5d697db72")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

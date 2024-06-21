00915d8292fdfecea0624a18a6671bedc5ba22c4
Marimari2342 <marianroj@hotmail.com.ar>
2024-06-21 12:21:05 -0300

    commit agregarexpediente razor

--- SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
+++ SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c6bab91bec6000d1bc3355fba165d4f5abe373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670fe0799c5c51b7513f19257a40ca0b224af7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
